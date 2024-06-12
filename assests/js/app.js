8c6492350bd369ea37b35ca77bb10509bbfe1f13
developertool45 <developertool45@gmail.com>
2024-06-13 00:05:07 +0530

    update

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,5 +1,4 @@
 let mybutton = document.getElementById("btn-back-to-top");
-let socialIcons = document.querySelector(".whatsap_button");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -11,11 +10,9 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
-    socialIcons.style.display = "block";
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
-    socialIcons.style.display = "none";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
@@ -26,15 +23,6 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-var typed = new Typed(".typing ", {
-  strings: ["Solution" ],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true
-});
-var typed1 = new Typed(".typing2 ", {
-  strings: ["Solution" ],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true
-});
+
+
+
